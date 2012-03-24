920d4e6f46bc8effca7992bf309384403849f742
Gregor Richards <Richards@codu.org>
2012-03-24 00:22:51 -0400

    Use host's malloc/realloc/free instead of relying on unportable brk, added some more syscalls.

--- libmicrocosm/syscall.c
+++ libmicrocosm/syscall.c
@@ -1,4 +1,7 @@
 /* this file only exists to provide symbols, the implementations are in
- * libmicrocosm_host.so.1 */
+ * libmicrocosm_host.so.1 or libc */
 void __syscall() {}
-void __syscall_cp_asm() {}
+void __syscall_cp() {}
+void malloc() {}
+void realloc() {}
+void free() {}
--- libmicrocosm_host/syscall.c
+++ libmicrocosm_host/syscall.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "mcerrno.h"
 #include "mcsyscall.h"
+#include "reerrno.h"
 #include "visibility.h"
 
 /* FIXME: this is a gross way to handle this */
-#define F(nm) long MC_##nm(long, ...)
-F(writev);
-#undef F
+#define FW(nm) long MC_##nm(long, ...)
+FW(writev);
+#undef FW
 
+/* wrapped syscalls (simple case) */
 #define W(nm) case MC_SYS_##nm: return MC_##nm(a, b, c, d, e, f)
+
+/* direct syscalls (simple case) */
+#define D(nm, bad) case MC_SYS_##nm: REERRNO(ret, nm, bad, (a, b, c, d, e, f)); return ret
 
 VISIBLE long __syscall(long n, long a, long b, long c, long d, long e, long f)
 {
+    long ret;
+
     /* FIXME: autogen in some way */
+    fprintf(stderr, "[microcosm] Syscall %ld.\n", n);
     switch (n) {
         /* wrapped calls */
         W(writev);
@@ -27,6 +37,7 @@ VISIBLE long __syscall(long n, long a, long b, long c, long d, long e, long f)
             break;
 
         /* unsupported syscalls */
+        case MC_SYS_brk: /* 12 */
         case MC_SYS_ioctl: /* 16 */
         case MC_SYS_set_tid_address: /* 218 */
             return -MC_ENOSYS;
@@ -34,8 +45,9 @@ VISIBLE long __syscall(long n, long a, long b, long c, long d, long e, long f)
         default:
             fprintf(stderr, "[microcosm] Unsupported syscall %ld.\n", n);
 #ifdef __linux__
-            fprintf(stderr, "[microcosm] Passing syscall to host.\n");
-            return syscall(n, a, b, c, d, e, f);
+            ret = syscall(n, a, b, c, d, e, f);
+            fprintf(stderr, "[microcosm] %ld(%lX, %lX, %lX, %lX, %lX, %lX) = %lX\n", n, a, b, c, d, e, f, ret);
+            return ret;
 #else
             fprintf(stderr, "[microcosm] Returning ENOSYS.\n");
             return -MC_ENOSYS;
