6643eae5c1dd6f045aaf16d700a7af4b4fda30fe
Gregor Richards <Richards@codu.org>
2012-03-28 09:15:52 -0400

    Use sysroot while building the native CC to avoid stupid build issues.

--- cross/buildcc-native.sh
+++ cross/buildcc-native.sh
@@ -20,9 +20,14 @@ GCC_VERSION=4.7.0
 
 # binutils
 fetchextract http://ftp.gnu.org/gnu/binutils/ binutils-$BINUTILS_VERSION .tar.bz2
-buildinstall n1 binutils-$BINUTILS_VERSION --host=$TRIPLE --target=$TRIPLE
+buildinstall n1 binutils-$BINUTILS_VERSION --host=$TRIPLE --target=$TRIPLE \
+    --with-sysroot="$MICROCOSM_NATIVE_PREFIX"
 
 # gcc 1
+if [ ! -e "$MICROCOSM_NATIVE_PREFIX"/usr ]
+then
+    ln -sf . "$MICROCOSM_NATIVE_PREFIX"/usr
+fi
 fetchextract http://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/ gcc-$GCC_VERSION .tar.bz2
 buildinstall n1 gcc-$GCC_VERSION --host=$TRIPLE --target=$TRIPLE \
-    --enable-languages=c
+    --enable-languages=c --with-sysroot="$MICROCOSM_NATIVE_PREFIX"
