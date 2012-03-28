2f099693a043546a0926efdd54ad909087fb2d2b
Gregor Richards <Richards@codu.org>
2012-03-28 08:59:16 -0400

    Use host calloc in libmicrocosm.

--- libmicrocosm/malloc.c
+++ libmicrocosm/malloc.c
@@ -14,6 +14,11 @@ VISIBLE MC_ABI void *microcosm__malloc(size_t size)
     return malloc(size);
 }
 
+VISIBLE MC_ABI void *microcosm__calloc(size_t nmemb, size_t size)
+{
+    return calloc(nmemb, size);
+}
+
 VISIBLE MC_ABI void *microcosm__realloc(void *ptr, size_t size)
 {
     return realloc(ptr, size);
