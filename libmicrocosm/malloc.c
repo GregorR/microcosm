792963d589161fd832ca352f8be455e8c15a2651
Gregor Richards <Richards@codu.org>
2012-03-28 23:33:11 -0400

    Doing some (hopefully portable and non-awful) fixes for malloc/realloc where malloc(0)==NULL.

--- libmicrocosm/malloc.c
+++ libmicrocosm/malloc.c
@@ -14,16 +14,26 @@ VISIBLE MC_ABI void microcosm__free(void *ptr)
 
 VISIBLE MC_ABI void *microcosm__malloc(size_t size)
 {
+#ifndef HAVE_GOOD_MALLOC
+    if (size == 0) size++;
+#endif
     return malloc(size);
 }
 
 VISIBLE MC_ABI void *microcosm__calloc(size_t nmemb, size_t size)
 {
+#ifndef HAVE_GOOD_MALLOC
+    if (nmemb == 0) nmemb++;
+    if (size == 0) size++;
+#endif
     return calloc(nmemb, size);
 }
 
 VISIBLE MC_ABI void *microcosm__realloc(void *ptr, size_t size)
 {
+#ifndef HAVE_GOOD_REALLOC
+    if (size == 0) size++;
+#endif
     return realloc(ptr, size);
 }
 
